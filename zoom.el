64381e748160d7f23fbd90d6171a4cc415817fec
Andrea Cardaci <cyrus.and@gmail.com>
2017-11-09 01:17:25 +0100

    Avoid updating the layout if the selected window is not changed
    Related to #13.

--- zoom.el
+++ zoom.el
@@ -165,7 +165,11 @@ used when this function is called via `advice-add'."
         (if (or track-mouse
                 (and zoom-minibuffer-preserve-layout (window-minibuffer-p)))
             (get-mru-window nil nil t) (selected-window))
-      (zoom--update))))
+      ;; update the layout only if the selected window has changed
+      (unless (and (boundp 'zoom--current-window)
+                   (eq zoom--current-window (selected-window)))
+        (setq zoom--current-window (selected-window))
+        (zoom--update)))))
 
 (defun zoom--update ()
   "Update the window layout in the current frame."
